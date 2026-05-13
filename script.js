e215717a5053a4048c64ee6381d09c3f6fa044e1
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-05-13 11:54:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let classes = document.getElementById('classes');
+console.log(classes)
+let numRand = Math.floor(Math.random() * 20);
+
+let valorDado = document.getElementById('valor-dado');
+valorDado.innerText
