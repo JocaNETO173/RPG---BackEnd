49b2828c4c0d57ab95daaf553c4115b3990cd82d
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-05-18 09:09:06 -0300

    Add files via upload
    Criamo as variáveis principais, a função do botão, uma verificação caso não escolha uma arma ou uma classe, e criamos uma variável para abrigar o resultado

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let numRand = Math.floor((Math.random() * 20)+1);
-// let numRand = 20;
+function lutar(){
+    const classe = document.getElementById('classes').value;
+    const arma = document.getElementById('arma').value.trim();
+    const log = document.getElementById('log');
+    const nome = document.getElementById('nome').value;
+
+    if(!arma || !classe){
+        log.innerHTML = "Você esqueceu de selecionar algo!";
+        return;
+    };
+    
+    const numRand = Math.floor((Math.random() * 20)+1);
 
 
 
-let valorDado = document.getElementById('valor-dado');
-if(numRand === 20){
-    valorDado.classList.add = 'critico';
+    let resultado  = `<strong>${nome}</strong> O <strong>${classe}</strong> com <strong>${arma}</strong>`
+    const valorDado = document.getElementById('valor-dado');
+    if(numRand === 20){
+        valorDado.classList.add('critico');
+
+    } else {
+        valorDado.classList.remove('critico');
+    }
+    valorDado.innerText = numRand
+
 }
-valorDado.innerText = numRand
-
-// 
